28863b61470b395f7e99a70d20e0225f95c8527b
steve <swahn@protonmail.com>
2018-11-16 17:57:12 -0500

    blog api controllers functional.

--- server/src/Controllers/BlogController.php
+++ server/src/Controllers/BlogController.php
@@ -20,11 +20,107 @@ class BlogController
     {
         try {
 
-            $cookie = '';//Cookie::get($request);
+            $cookie = Cookie::get($request);
 
             $model = new Blog();
 
             $data = $model->get($cookie);
+
+            $code = $response->getStatusCode();
+
+            return $response->withJson($data, $code);
+
+        } catch (\Exception $e) {
+
+            $error = $e->getMessage();
+
+            $code = $e->getCode();
+
+            return $response->withJson($error, $code);
+        }
+    }
+
+    public function getOne(Request $request, Response $response, array $args) : Response
+    {
+        try {
+
+            $cookie = Cookie::get($request);
+
+            $model = new Blog();
+
+            $data = $model->getOne($request, $args, $cookie);
+
+            $code = $response->getStatusCode();
+
+            return $response->withJson($data, $code);
+
+        } catch (\Exception $e) {
+
+            $error = $e->getMessage();
+
+            $code = $e->getCode();
+
+            return $response->withJson($error, $code);
+        }
+    }
+
+    public function post(Request $request, Response $response) : Response
+    {
+        try {
+
+            $cookie = Cookie::get($request);
+
+            $model = new Blog();
+
+            $data = $model->post($request, $cookie);
+
+            $code = $response->getStatusCode();
+
+            return $response->withJson($data, $code);
+
+        } catch (\Exception $e) {
+
+            $error = $e->getMessage();
+
+            $code = $e->getCode();
+
+            return $response->withJson($error, $code);
+        }
+    }
+
+    public function put(Request $request, Response $response, array $args) : Response
+    {
+        try {
+
+            $cookie = Cookie::get($request);
+
+            $model = new Blog();
+
+            $data = $model->put($request, $args, $cookie);
+
+            $code = $response->getStatusCode();
+
+            return $response->withJson($data, $code);
+
+        } catch (\Exception $e) {
+
+            $error = $e->getMessage();
+
+            $code = $e->getCode();
+
+            return $response->withJson($error, $code);
+        }
+    }
+
+    public function delete(Request $request, Response $response, array $args) : Response
+    {
+        try {
+
+            $cookie = Cookie::get($request);
+
+            $model = new Blog();
+
+            $data = $model->delete($args, $cookie);
 
             $code = $response->getStatusCode();
 
--- server/src/Models/Blog.php
+++ server/src/Models/Blog.php
@@ -8,9 +8,9 @@ namespace src\Models;
 
 class Blog extends Model
 {
-    public function get($cookie)
+    public function get($cookie) : array
     {
-        $sql = '';
+        $sql = "";
 
         $values = [];
 
@@ -21,4 +21,84 @@ class Blog extends Model
 
         //return parent::call($sql, $values);
     }
+
+    public function getOne($request, $args, $cookie) : array
+    {
+        $sql = "";
+
+        $values = [];
+
+        return [
+            'getOne' => 'blog api getOne request.',
+            'time()' => time()
+        ];
+        //return parent::call($sql, $values);
+    }
+
+    public function post($request, $cookie) : array
+    {
+        $sql = "";
+
+        $params = $request->getParsedBody();
+
+        $time = time();
+
+        if (!isset($params['subject']) || empty($params['subject'])) {
+            throw new \Exception('A subject is required.', 400);
+        }
+
+        if (!isset($params['message']) || empty($params['message'])) {
+            throw new \Exception('A messate is required.', 400);
+        }
+
+        $values = [
+            'subject' => $params['subject'],
+            'message' => $params['message']
+        ];
+
+        return $params;
+
+        //return parent::call($sql, $values);
+    }
+
+    public function put($request, $args, $cookie) : array
+    {
+        $sql = "";
+
+        $params = $request->getParsedBody();
+
+        $time = time();
+
+        if (!isset($params['subject']) || empty($params['subject'])) {
+            throw new \Exception('A subject is required.', 400);
+        }
+
+        if (!isset($params['message']) || empty($params['message'])) {
+            throw new \Exception('A messate is required.', 400);
+        }
+
+        $values = [
+            'subject' => $params['subject'],
+            'message' => $params['message']
+        ];
+
+        return $params;
+
+        //return parent::call($sql, $values);
+    }
+
+    public function delete($args, $cookie) : array
+    {
+        $sql = "";
+
+        $values = [];
+
+        return [
+            'status' => 'ok',
+            'code' => 200
+        ];
+
+        //return parent::call($sql, $values);
+    }
+
 }
